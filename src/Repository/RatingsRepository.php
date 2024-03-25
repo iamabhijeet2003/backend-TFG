0f62605c337aa86408a6a4b0a41732baad05b76f
abhijeet <abhijeet3016@gmail.com>
2024-03-25 15:14:54 +0100

    adding rating entity repo

---
+++ src/Repository/RatingsRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Ratings;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Ratings>
+ *
+ * @method Ratings|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Ratings|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Ratings[]    findAll()
+ * @method Ratings[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RatingsRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Ratings::class);
+    }
+
+//    /**
+//     * @return Ratings[] Returns an array of Ratings objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('r.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Ratings
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
