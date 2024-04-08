fc0f229e0c7aa204b83b96f7731cf63c6212598b
abhijeet <abhijeet3016@gmail.com>
2024-04-08 15:52:30 +0200

    add migration of the modification to the order table

---
+++ migrations/Version20240406214125.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240406214125 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `order` ADD address VARCHAR(255) DEFAULT NULL, ADD city VARCHAR(255) DEFAULT NULL, ADD postal_code INT DEFAULT NULL, ADD country VARCHAR(255) DEFAULT NULL, ADD mobile INT DEFAULT NULL, ADD province VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `order` DROP address, DROP city, DROP postal_code, DROP country, DROP mobile, DROP province');
+    }
+}
